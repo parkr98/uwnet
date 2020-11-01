f636fd234d046d2c01672ad03b65751d826fa378
Richard Park <parkr98@attu1.cs.washington.edu>
2020-11-01 00:14:43 -0700

    Final commit

--- tryhw1.py
+++ tryhw1.py
@@ -1,5 +1,27 @@
 from uwnet import *
 
+# filters * c * size^2 * im.w * im.h / stride^2
+# layer 1: 32*32*3*8*9=221184
+# layer 2: 16*16*8*16*9=294912
+# layer 3: 8*8*16*32*9=294912
+# layer 4: 4*4*32*64*9=294912
+# layer 5: 256*10=2560
+# convnet total: 1,108,480
+# connected total: 1,082,624
+
+def connected_net():
+    l = [   make_connected_layer(3072, 256),
+            make_activation_layer(RELU),
+            make_connected_layer(256, 512),
+            make_activation_layer(RELU),
+            make_connected_layer(512, 256),
+            make_activation_layer(RELU),
+            make_connected_layer(256, 128),
+            make_activation_layer(RELU),
+            make_connected_layer(128, 10),
+            make_activation_layer(SOFTMAX)]
+    return make_net(l)
+
 def conv_net():
     l = [   make_convolutional_layer(32, 32, 3, 8, 3, 1),
             make_activation_layer(RELU),
@@ -30,7 +52,7 @@ rate = .01
 momentum = .9
 decay = .005
 
-m = conv_net()
+m = connected_net()
 print("training...")
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
@@ -44,4 +66,14 @@ print("test accuracy:     %f", accuracy_net(m, test))
 # Why are you seeing these results? Speculate based on the information you've gathered and what you know about DL and ML.
 # Your answer:
 #
+# convnet training accuracy:   0.6756600141525269
+# convnet test accuracy:       0.6328999996185303
+#
+# connected training accuracy: 0.5573800206184387
+# connected test accuracy:     0.5051000118255615
+#
+# Clearly, our convnet performed better because with equal number of operations, the convnet is able to extract
+# more useful features through the spatial assumption made in the convolution layers. Although, in theory, fully
+# connected networks are just as powerful as convnet, without this spatial assumption, my connected network likely
+# wasn't able to converge to a lower local minimum than what convnet was able to.
 
